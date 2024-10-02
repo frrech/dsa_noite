b3835f405d43a0b53019428d52684fe91392d0fc
FRANCISCO IBARGOYEN RECH <frrech@outlook.com>
2024-10-01 21:02:07 -0300

    Teste Express.js

---
+++ aula08/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/produtos', (req, res) =>{
+    res.send('Listando produtos!')
+})
+
+app.post('/produtos', (req, res) =>{
+  res.send('Inserindo produtos!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
